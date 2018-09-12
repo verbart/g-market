24c906f20b88c4086d89b85ef67e6edca05b3a85
Leonid Zychkov <buldog52@gmail.com>
2018-09-13 00:09:31 +0300

    Started slider in slide1

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'slick-carousel';
 
 
 import './components/main-content/main-content'
+import './components/announcement/announcement'
 import './components/menu-toggle/menu-toggle'
 import './components/modal/modal';
 import './components/form/form';
---
+++ src/components/announcement/announcement.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+
+$('.announcement__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  infinite: true,
+  asNavFor: '.announcement__slider-preview'
+});
+
+$('.announcement__slider-preview').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  infinite: true,
+  asNavFor: '.announcement__slider',
+  focusOnSelect: true
+});
+
+
